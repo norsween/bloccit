2149f3b108054ce232f34c107d9964d32f9bcad7
norsween <norsween@gmail.com>
2018-06-14 05:16:26 +0100

    implemented a TDD route that accepts a GET request to /marco

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,12 +1,29 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marcoBase = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
   describe("GET /", () => {
     it("should return status code 200", (done) => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        done();
+      });
+    });
+  });
+
+  describe("GET /", () => {
+    it("should return status code 200", (done) => {
+      request.get(marcoBase, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    it("should return string 'Polo'", (done) => {
+      request.get(marcoBase, (err, res, body) => {
+        expect(body).toContain('Polo');
         done();
       });
     });
--- src/routes/static.js
+++ src/routes/static.js
@@ -5,4 +5,8 @@ router.get("/", (req, res, next) => {
   res.send("Welcome to Bloccit");
 });
 
+router.get("/marco", (req, res, next) => {
+  res.send("Polo");
+});
+
 module.exports = router;

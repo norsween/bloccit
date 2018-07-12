5023f0b4adeb49c086e3ec74b14a6464c2546237
norsween <norsween@gmail.com>
2018-07-12 01:50:03 +0100

    implemented assignment to create an upvote automatically for a post

--- spec/integration/votes_spec.js
+++ spec/integration/votes_spec.js
@@ -74,7 +74,7 @@ describe("routes : votes", () => {
 
      describe("GET /topics/:topicId/posts/:postId/votes/upvote", () => {
 
-       it("should not create a new vote", (done) => {
+       it("should create an automatic new vote", (done) => {
          const options = {
            url: `${base}${this.topic.id}/posts/${this.post.id}/votes/upvote`
          };
@@ -87,7 +87,7 @@ describe("routes : votes", () => {
                }
              })
              .then((vote) => {
-               expect(vote).toBeNull();
+               expect(vote).not.toBeNull();
                done();
              })
              .catch((err) => {
--- src/db/models/post.js
+++ src/db/models/post.js
@@ -47,6 +47,14 @@ module.exports = (sequelize, DataTypes) => {
          postId: post.id 
        });
      });
+
+     Post.afterCreate((post, callback) => {
+       return models.Vote.create({
+         userId: post.userId,
+         postId: post.id,
+	 value: 1 
+       });
+     });
    };
 
    Post.prototype.getPoints = function(){
